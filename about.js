f49c9e583d21a11cab9b052d5957134a5c385429
José Ivo Koerich Nery <joseivokoerichnery@gmail.com>
2022-01-20 18:52:46 -0300

    Adição do Sobre Mim

---
+++ about.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $("#ABOUT").text(getText());
+
+
+  });
+
+function getText() {
+    return 'Graduando em Sistemas de Informação pela Unifacisa. Sempre procuro a oportunidade de crescimento em conjunto '  +
+    'com o time e crescimento pessoal também a fim de que possa chegar ao objetivo comum sendo elas a entrega e '+
+    'satisfação do cliente em ter um produto de alta qualidade. '+
+    'Ansioso em aprender, em saber das coisas e como essas coisas acontecem, bom em resolver problemas, não '+
+    'sou uma pessoa perfeita, mas sabe que existe hora para tudo, e se não sabe fazer algo tenho pulso para dizer e pedir '+
+    'ajuda, aprendo com erros e aceito críticas sendo elas bem fundamentadas. '+
+    'Em conjunto ou equipe gosta de conhecer todo mundo, inteirar-se das atividades de maneira que consiga '+
+    'visualizar impactos futuros de decisões tomadas hoje. No final dando tudo certo sabe compartilhar o momento, porém '+
+    'se algo der errado mantém o espirito de união.';
+}
