b5c078d4c334fe86e5e5ad4779af5b17525b514f
José Ivo Koerich Nery <joseivokoerichnery@gmail.com>
2022-01-21 00:04:23 -0300

    Versão 2.0

---
+++ assets/js/about.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $("#ABOUT").text(getText());
+
+  });
+
+function getText() {
+    return "Graduating in Information Systems at Unifacisa. I always look for the opportunity for growth together " + 
+    "with the team and personal growth so that I can reach the common goal, which is delivery and customer satisfaction "+
+    "in having a high quality product. Eager to learn, to know about things and how these things happen, good at solving problems, "+
+    "I'm not a perfect person, but I know there is a time for everything, and if I don't know how to do something, I have the pulse "+
+    "to say and ask for help, I learn from mistakes and I accept criticisms being well founded. " +
+    "As a group or team, he likes to get to know everyone, to learn about activities in a way that he can visualize future impacts "+
+    "of decisions made today. In the end, everything works out, you know how to share the moment, but if something goes wrong, keep the spirit of union.";
+}
